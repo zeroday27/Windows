dd1ef2bfffddb4142f5294b720f4aea5d49fd47d
Ko Ko Naing <86004494+zeroday27@users.noreply.github.com>
2022-12-13 13:06:13 +0630

    Add Windows AD User PowerShell Scripts

---
+++ UserCreate.ps1
@@ -0,0 +1,27 @@
+﻿Import-Module ActiveDirectory
+
+$filepath = Read-Host -Prompt "csv filepath"
+
+$users = Import-Csv $filepath
+
+ForEach ($user in $users) {
+    $loginname = $user.username + "@sapb1mm.com"
+    $fname = $user.firstname
+    $lname = $user.lastname
+    $uname = $user.username
+    $password = $user.password
+    $OU = $user.ou
+    New-ADUser `
+    -Name "$fname $lname" `
+    -SamAccountName $uname `
+    -GivenName $fname `
+    -Surname $lname `
+    -UserPrincipalName $loginname `
+    -Path $OU `
+    -AccountPassword (ConvertTo-SecureString $password -AsPlainText -Force) `
+    -ChangePasswordAtLogon $False `
+    -CannotChangePassword $True `
+    -PasswordNeverExpires $True `
+    -Enable $True
+
+}
